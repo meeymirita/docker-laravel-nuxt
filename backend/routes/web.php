8150f8e31812a15d69674adad623c7b5a9703cb6
mirita <popap182@gmail.com>
2025-10-25 22:57:37 +0300

    # 1 test web

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\TestRabbitJob;
 
+Route::get('/test-rabbit', function () {
+    TestRabbitJob::dispatch('Hello from RabbitMQ!');
+    return 'Job dispatched to RabbitMQ!';
+});
 Route::get('/', function () {
     return view('welcome');
 });
