c739c1deb2257b51d500a3f03ef99ac5cbc7ae1a
mirita <popap182@gmail.com>
2025-10-25 20:09:28 +0300

    # 3 end redis test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Redis
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
