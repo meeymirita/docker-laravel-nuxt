cbb5a4c04b1b54401ba105931d25879c8f937e3d
mirita <popap182@gmail.com>
2025-10-27 16:33:29 +0300

    # 5 clear

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,16 +1,2 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
-use App\Jobs\TestRabbitJob;
-
-Route::get('/test-rabbit', function () {
-    TestRabbitJob::dispatch('Hello from RabbitMQ!');
-    return 'Job dispatched to RabbitMQ!';
-});
-Route::get('/', function () {
-    return view('welcome');
-});
-// Redis
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
