3715b6c0fc4c2e71857c7b8c32377fe3845eb6d9
mirita <popap182@gmail.com>
2025-11-02 21:36:18 +0300

    # 2 fix and clear files

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('user')->name('user.')->group(function () {
     Route::post('/register', [UserController::class, 'register'])->name('register');
 });
-
+//q
 // подтверждение емаил
 Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
     ->middleware('signed')
