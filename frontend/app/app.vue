7c4bd017038205cf4a8cd5e5478c2cac7ca7965e
miraykim <miraykim101@gmail.com>
2025-11-08 22:38:04 +0700

    fix image for bg

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -58,7 +58,4 @@
   text-shadow: 0 0 5px white;
 }
 
-body {
-  padding-top: 80px;
-}
 </style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            link: [
+                // Подключение Google Fonts
+                { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+                { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@100..900&family=Turret+Road:wght@200;300;400;500;700;800&display=swap'
+                }
+            ]
+        }
+    },
     css: [
         './app/assets/styles/main.css'
     ],
