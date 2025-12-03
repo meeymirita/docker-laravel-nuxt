9024ce98c509b22a3bea81315365cf0ca8fb22a2
mirita <popap182@gmail.com>
2025-12-03 17:15:38 +0300

    # fix 2

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  css: [
-      '~/assets/styles/main.scss'
-  ],
+    css: [
+        '~/assets/styles/main.scss'
+    ],
 
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/icon']
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxt/icon'],
+
+
+    server: {
+        host: '0.0.0.0',
+        port: 3000
+    }
 })
