e02ab102052fb055164c0ed7f26fe92041b0f869
mirita <popap182@gmail.com>
2025-12-03 17:34:12 +0300

    # fix 4

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,34 +1,18 @@
 export default defineNuxtConfig({
     css: ['~/assets/styles/main.scss'],
 
-    nitro: {
-        preset: 'node-server',
-        serveStatic: true,
-        serverAssets: [
-            { baseName: 'public', dir: './public' }
-        ]
-    },
-
     devServer: {
-        host: '0.0.0.0',
+        host: '0.0.0.0',          // слушать на всех интерфейсах
         port: 3000,
-        allowedHosts: ['meeymirita.ru', 'localhost'] // <--- разрешаем твой домен
+        allowedHosts: ['meeymirita.ru', 'localhost', '0.0.0.0']
     },
 
-    runtimeConfig: {
-        public: {
-            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'http://meeymirita.ru'
-        }
-    },
-
-    compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
-    modules: ['@nuxt/icon'],
+    nitro: { preset: 'node-server', serveStatic: true },
 
     vite: {
         server: {
             host: true,
-            allowedHosts: ['meeymirita.ru', 'localhost'] // <--- тоже нужно для Vite
+            allowedHosts: ['meeymirita.ru', 'localhost', '0.0.0.0']
         }
     }
 })
